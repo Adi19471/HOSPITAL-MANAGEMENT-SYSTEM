dd68bc4d79c97c07831285556a52d4c5cb5c5b16
adi <akumatha@gmail.com>
2021-11-12 18:04:45 +0100

    home page

--- HospitalAPP/models.py
+++ HospitalAPP/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+# HOSPITAL DATA BASE 
+
+# auth group ------------ 1
+class auth_group(models.Model):
+
+    name = models.CharField(max_length=120)
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
